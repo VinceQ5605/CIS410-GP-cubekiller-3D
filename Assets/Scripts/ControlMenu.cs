7f3c9c68d543dadb92eb507338b7cdacf67f5182
Vince Qiu <rexqiu5605@gmail.com>
2022-05-24 00:22:00 -0700

    Start menu and Pause menu, Pause menu current  in TempScene

---
+++ Assets/Scripts/ControlMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlMenu : MonoBehaviour
+{
+    public void SetXaxis(float Xaxis)
+    {
+
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+        if (GameIsPaused && pauseMenuUI != null)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else 
+        { 
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+}
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+}
