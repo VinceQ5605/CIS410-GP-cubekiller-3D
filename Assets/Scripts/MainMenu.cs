c4f3e20a48f123a3439560868c5a3636b643247e
Vince Qiu <rexqiu5605@gmail.com>
2022-05-25 00:48:41 -0700

    Connected all three scenes to the main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject pauseMenuUI;
-    public void PlayGame()
+    public void PlayMountainW()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (PauseMenu.GameIsPaused)
+        {
+            Time.timeScale = 1f;
+            PauseMenu.GameIsPaused = false;
+        }
+    }
+
+    public void PlayDesertW()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        if (PauseMenu.GameIsPaused)
+        {
+            Time.timeScale = 1f;
+            PauseMenu.GameIsPaused = false;
+        }
+    }
+
+    public void PlaySnowW()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
         if (PauseMenu.GameIsPaused)
         {
             Time.timeScale = 1f;
