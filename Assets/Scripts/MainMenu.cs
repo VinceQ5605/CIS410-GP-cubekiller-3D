af6f2a6c862c135b6de376ac5c6a4af130a73492
Vince Qiu <rexqiu5605@gmail.com>
2022-05-24 20:03:29 -0700

    Fixed pause menu and connected both start and pause menu to the main game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject pauseMenuUI;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (PauseMenu.GameIsPaused)
+        {
+            Time.timeScale = 1f;
+            PauseMenu.GameIsPaused = false;
+        }
     }
 
     public void QuitGame()
