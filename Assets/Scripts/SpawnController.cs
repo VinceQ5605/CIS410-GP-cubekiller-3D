0f7df7f828ac0d0ea4e581df2a52885144a737ea
jakepotter <36420045+jakepotter@users.noreply.github.com>
2022-06-03 06:38:48 -0700

    Set up transformation points

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -12,6 +12,8 @@ public class SpawnController : MonoBehaviour
     private GameObject baseObject;
     private List<GameObject> transformationLocations;
     private GameObject coinPrefab;
+    private GameObject destination1;
+    private GameObject destination2;
 
 
     public void SetValues(GameObject enemyPrefab, GameObject baseObject, List<GameObject> transformationLocations, GameObject coinPrefab)
@@ -20,6 +22,40 @@ public class SpawnController : MonoBehaviour
         this.baseObject = baseObject;
         this.transformationLocations = transformationLocations;
         this.coinPrefab = coinPrefab;
+
+        destination1 = transformationLocations[0];
+        destination2 = transformationLocations[1];
+        Vector3 pos = transform.position + (new Vector3(.1f, 0f, .2f)); // just to make sure not everything is the same distance apart
+        Vector3 delta1 = transformationLocations[0].transform.position - pos;
+        Vector3 delta2 = transformationLocations[1].transform.position - pos;
+        float dot1 = Vector3.Dot(delta1, delta1);
+        float dot2 = Vector3.Dot(delta2, delta2);
+        if (dot1 > dot2)
+        {
+            float temp = dot1;
+            dot1 = dot2;
+            dot2 = temp;
+            destination1 = transformationLocations[1];
+            destination2 = transformationLocations[0];
+        }
+
+        for (int i = 2; i < transformationLocations.Count; i++)
+        {
+            Vector3 delta = transformationLocations[i].transform.position - pos;
+            float dot = Vector3.Dot(delta, delta);
+            if (dot < dot1)
+            {
+                dot2 = dot1;
+                destination2 = destination1;
+                dot1 = dot;
+                destination1 = transformationLocations[i];
+            }
+            else if (dot < dot2)
+            {
+                dot2 = dot;
+                destination2 = transformationLocations[i];
+            }
+        }
     }
 
     public void Spawn(int numberOfEnemies)
@@ -38,7 +74,12 @@ public class SpawnController : MonoBehaviour
         {
             GameObject newEnemy = Instantiate(enemyPrefab, position, Quaternion.identity);
             EnemyController enemyController = newEnemy.GetComponent<EnemyController>();
-            enemyController.SetDestination(transformationLocations[0]);
+            GameObject destination = destination1;
+            if (i % 2 == 0)
+            {
+                destination = destination2;
+            }
+            enemyController.SetDestination(destination);
             newEnemy.SetActive(true);
             if (i%5 == 0)
                 enemyController.isCarryingCoin = true;
